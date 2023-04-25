b60eb1ccea7350ad5dc590fdf841cb012651a9a4
Kimtra <112887208+Tea-2003@users.noreply.github.com>
2023-04-25 17:50:54 +0700

    Add files via upload

---
+++ BT Form/Data.js
@@ -0,0 +1,17 @@
+
+export function Data() {
+    return [
+      {
+        name: "cà phê sữa",
+        price: 40000,  
+      },
+      {
+        name: "cà phê đen",
+        price: 45000,  
+      },
+      {
+        name: "nước siro",
+        price: 50000,  
+      },
+    ]
+}
